4a6005df100e2f657458080f672cb760aa8216aa
dmitry-bakeev <bakeevdimon@gmail.com>
2021-12-29 22:43:45 +0500

    Some fix

--- blogs/tasks.py
+++ blogs/tasks.py
@@ -6,5 +6,10 @@ from .services import send_html_email
 
 @app.task
 def send_email_to_subscribers(post_pk):
-    post = Post.objects.get(pk=post_pk)
+    post = None
+    while not post:
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            pass
     send_html_email(post)
